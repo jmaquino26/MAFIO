abb35900a716f8d67fa64b2d654a886917126be4
Francis Windows-PC <francisgian_fgm100@outlook.com>
2021-05-13 03:36:28 +0800

    changed the functionality of ecord_tweeted_frames
    * when the frame is recorded, python will move the file into TWEETED_FRAMES_FOLDER which is defined in line 8 you can change that
    
    * implemented queue data structure (FIRST IN FIRST OUT) When a frame is tweeted the frame will be popped from the left of the list

--- MAFIO.py
+++ MAFIO.py
@@ -1,9 +1,10 @@
 import os
 
 import tweepy
-
 # load environmental variables
 from dotenv import load_dotenv
+from utils import utils
+
 load_dotenv()
 
 # API KEYS
@@ -18,6 +19,35 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def main():
+    """Main function"""
+
+    # THIS IS WHERE THE WHILE LOOP IS GONNA RUN!
+
+    # implement a queue
+
+    # WHILE TRUE:
+    frame_list = utils.list_all_frames()
+
+    while True:
+        for frame in frame_list:
+
+            api.update_status()
+
+            # desired sleep time
+
+            # mb add this to line 34
+
+            # when tweeted dump the frame into another folder
+            utils.record_tweeted_frames(frame_list.popleft())
+
+
+    # API.UPDATE_STATUS()
+
+    # THEN 
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,20 @@
 import os
+import shutil
 
+from collections import deque
+
+# CONSTANTS
 MISS_AMERICANA_FRAMES_FOLDER = 'C:\\Users\\franc\\OneDrive\\Pictures\\MissAmericana'  # noqa
+TWEETED_FRAMES_FOLDER = 'C:\\Users\\franc\\OneDrive\\Pictures\\MissAmericanaTweetedFrames'  # noqa
+
 CWD = os.getcwd()
 PROGRESS_FOLDER = os.path.join(CWD, 'saved_progress')
 RECORDED_FRAMES_FILE = os.path.join(PROGRESS_FOLDER, 'recorded_frames.txt')
 
 
-def list_all_frames(path: str) -> list:
+def list_all_frames() -> deque:
     """List all photos inside the path"""
-    return os.listdir(path)
+    return deque(os.listdir(MISS_AMERICANA_FRAMES_FOLDER))
 
 
 def _ensure_folder():
@@ -16,26 +22,54 @@ def _ensure_folder():
     if not os.path.exists(PROGRESS_FOLDER):
         os.mkdir(PROGRESS_FOLDER)
 
+    if not os.path.exists(TWEETED_FRAMES_FOLDER):
+        os.mkdir(TWEETED_FRAMES_FOLDER)
+
 
 def record_tweeted_frames(frame_number) -> None:
 
     _ensure_folder()
 
     with open(RECORDED_FRAMES_FILE, 'a') as f:
-        f.write(str(frame_number))
-        f.write('\n')
+        if isinstance(frame_number, list):
+
+            for frame in frame_number:  # (s)
+                num, _ = os.path.splitext(frame)
+                f.write(str(num))
+                f.write('\n')
+        else:
+            f.write(str(frame_number))
+            f.write('\n')
+
+    # moves the frame to other folder when finished tweeting
+    # so it will not load/list the frames again from 0000
+    shutil.move(f'{MISS_AMERICANA_FRAMES_FOLDER}\\{frame_number}',
+                f'{TWEETED_FRAMES_FOLDER}\\{frame_number}')
+
+
+def recorded_tweet_list() -> list:
+    """Returns the list of frame numbers that's already been tweeted
+    NOTE: the last index is always an empty string ('')
+
+    Returns:
+        list: list of frames in the `RECORDED_FRAMES_FILE: CONSTANT`
+    """
+    with open(RECORDED_FRAMES_FILE) as f:
+        frames = [frame for frame in f.read().split('\n')]
+    return frames
 
 
 def get_last_recorded_tweet():
-    _ensure_folder()
-    frames = []
-    with open(RECORDED_FRAMES_FILE) as f:
+    """Gets the last Recorded Tweet
 
-        for num in f.read().split('\n'):
-            frames.append(num)
+    Returns:
+        str: Returns the last/`index[-2]` of `recorded_tweet_list`
+    """
+    _ensure_folder()
+    frames = recorded_tweet_list()
     return frames[-2]
 
 
 if __name__ == '__main__':
-    for frame in list_all_frames(MISS_AMERICANA_FRAMES_FOLDER):
-        record_tweeted_frames(str(frame[:-4]))
+    frames = list_all_frames()
+    
